b1d853ca44fae5bd8747ff9aa649053a5dd99b00
Suryansh Shukla <149030236+Suryansh703@users.noreply.github.com>
2026-08-06 18:18:28 +0530

    Time: 1 ms (100%), Space: 42.8 MB (30.89%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -3,21 +3,21 @@ class Solution {
         int i;
         for( i=n;i<=100;i++)
         {  
-            int sum=sum(i);
-            if(sum%t==0)
+            int pro=pro(i);
+            if(pro%t==0)
              break;
         }
          return i;
         
     }
-    int sum(int i)
-    {   int sum =1;
+    int pro(int i)
+    {   int pro =1;
         while(i!=0)
             {
                 int r=i%10;
-                sum*=r;
+                pro*=r;
                 i=i/10;
             }
-            return sum;
+            return pro;
     }
 }
